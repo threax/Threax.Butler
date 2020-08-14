eded0977d9b9322ba6474df512e803b1e8810e42
Andrew Piper <andrew@threax.com>
2020-08-14 10:25:55 -0400

    Updated nuget packages to get rid of old 2.2 libraries.

--- Butler/Startup.cs
+++ Butler/Startup.cs
@@ -221,7 +221,7 @@ namespace Butler
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
