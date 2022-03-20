2553abbb4229ebd40b7861681ed1cade2d2ec109
Andrew Piper <andrew@threax.com>
2022-03-20 09:19:50 -0400

    Update to net6.0.

--- Butler/Startup.cs
+++ Butler/Startup.cs
@@ -23,7 +23,6 @@ using Threax.AspNetCore.UserBuilder;
 using Threax.AspNetCore.UserLookup.Mvc.Controllers;
 using Threax.Extensions.Configuration.SchemaBinder;
 using System.Threading.Tasks;
-using Threax.Sqlite.Ext.EfCore3;
 
 namespace Butler
 {
@@ -167,7 +166,6 @@ namespace Butler
                 .AddTool("migrate", new ToolCommand("Migrate database to newest version. Run anytime new migrations have been added.", async a =>
                 {
                     await a.Migrate();
-                    a.Scope.ServiceProvider.GetRequiredService<AppDbContext>().ConvertToEfCore3();
                 }))
                 .AddTool("seed", new ToolCommand("Seed database data. Only needed for an empty database.", async a =>
                 {
